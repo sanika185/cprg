4d897da50cf9b714cf6290ca3de41b0061602246
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-24 08:41:59 +0530

    Add files via upload

---
+++ example2.c
@@ -0,0 +1,12 @@
+//Ass: write to print student name, branch, scored percentage and grade//
+
+#include<stdio.h>
+
+void main()
+
+{
+	printf("\n %s \t %s \t %d \t %c","Riya","science", 90,'A');
+	printf("\n %s \t %s \t %d \t %c","Maya","comerse", 80,'B');
+	printf("\n %s \t %s  %12d \t %c","Jiya","arts", 80,'c');
+}
+
