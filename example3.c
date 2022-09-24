ed5a498a791c306e1fddbb9ea2318cced6e078c2
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-24 08:42:22 +0530

    Add files via upload

---
+++ example3.c
@@ -0,0 +1,23 @@
+//Ass:wap to declare one integer,one decimal number and one character. save value input.//
+
+#include<stdio.h>
+
+void main()
+
+{
+	int x1;
+	float x2;
+	char x3;
+	
+	printf("\n enter integer:");
+	scanf("%d",&x1);
+	
+	printf("\n enter decimal no:");
+	scanf("%f",&x2);
+	
+	printf("\n enter chracter:");
+	scanf("%s",&x3);
+	
+	printf("\n x1=%d \t x2=%f \t x3=%s",x1,x2,x3);
+}
+ 
