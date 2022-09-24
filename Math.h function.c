9cc8ebb8144a0e43fb54e3850bac4835937d0f1d
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-24 11:55:59 +0530

    Add files via upload

---
+++ Math.h function.c
@@ -0,0 +1,29 @@
+//    Math.h function:   //
+
+#include<Math.h>
+
+void main()
+
+{
+	//1.pow:Find nth power of any number
+	printf("\n nth power =%lf",pow(5,2));
+	
+	//2.sqrt:Find squareroot
+	printf("\n Square root of number=%.2lf",sqrt(4));
+	
+	//3.floor:Find nearest minimal integer
+	printf("\n Round up of a number to previous int=%lf",floor(5.6));
+	
+	//4.ceil:find nearest next integer
+	printf("\n Round up of a number to next int=%lf",ceil(5.6));
+	
+	//5.log10:
+	printf("\n Find log value of a number=%lf",log10(199));
+	
+	//6.sin:
+	printf("\n Find sin value of number=%lf",sin(30));
+	
+	//7.exp:
+	printf("\n Exponential number=%E",exp(56));
+	
+}
