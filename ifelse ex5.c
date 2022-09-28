0c96d09d1ea8adfd791da71741d95ec44b615f09
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-28 09:32:02 +0530

    Add files via upload

---
+++ ifelse ex5.c
@@ -0,0 +1,24 @@
+//Ass:wap to input two numbers. check which is max.//
+
+#include<stdio.h>
+
+void main()
+
+{
+	int a,b;
+	
+	printf("\n Enter value of a:");
+	scanf("%d",&a);
+	
+	printf("\n Enter value of b:");
+	scanf("%d",&b);
+	
+	if(a>b)
+	{
+		printf("\n a is max");
+	}
+	else
+	{
+		printf("\n b is max");
+	}
+}
