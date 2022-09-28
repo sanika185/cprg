5c9581c1990dbbc86facd69fc893d5895d804014
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-28 09:31:12 +0530

    Add files via upload

---
+++ Ifelse ex3.c
@@ -0,0 +1,23 @@
+//Ass:wap to input any number. Check whether it is even or odd.
+
+#include<stdio.h>
+
+void main()
+
+{
+	int no;
+	
+	printf("\n Enter any number:");
+	scanf("%d",&no);
+	
+	
+	if(no%2==0)
+	{
+		printf("\n number is even");
+	}
+	else
+	{
+		printf("\n number is odd");
+	}
+	
+}
