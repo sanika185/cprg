b539fec9543c82a514f725d802617b1e243da72b
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-30 13:31:24 +0530

    Add files via upload

---
+++ assignment1 ex19.c
@@ -0,0 +1,18 @@
+//Ass:wap to find whether a given number is even or odd using conditional operator.
+
+#include<stdio.h>
+void main()
+
+{
+	int no;
+	printf("\n Enter any number:");
+	scanf("%d",&no);
+	if(no%2==0)
+	{
+		printf("\n Number is even");
+	}
+	else
+	{
+		printf("\n Number is odd");
+	}
+}
