c8b93ff57745018ba70d127fe346a39e7858704a
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-30 13:25:52 +0530

    Add files via upload

---
+++ assignment1 ex4.c
@@ -0,0 +1,13 @@
+//Ass:wap to input any number and print its cube//
+
+#include<stdio.h>
+void main()
+
+{
+	int no,cube;
+	
+	printf("\n Enter number:");
+	scanf("%d",&no);
+	cube=no*no*no;
+	printf("\n cube=%d",cube);
+}
