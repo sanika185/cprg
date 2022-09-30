40a2c9b979a415f4cf17974b2dfbe71e9313bec4
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-30 13:25:33 +0530

    Add files via upload

---
+++ assignment1 ex3.c
@@ -0,0 +1,12 @@
+//Ass:wap to find out square of the given no//
+
+#include<stdio.h>
+void main()
+{
+	int no,square;
+	
+	printf("\n Enter number:");
+	scanf("%d",&no);
+	square=(no)*no;
+	printf("\n Square=%d",square);
+}
