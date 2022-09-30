cd4bc05e0f624ba0fd29ae7b92620f92a0376b9f
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-30 13:29:56 +0530

    Add files via upload

---
+++ assignment1 ex15.c
@@ -0,0 +1,20 @@
+// Ass: wap to input two numbers and print the greatest using conditional operater
+
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("\n Enter first number:");
+	scanf("%d",&a);
+	printf("\n Enter second number:");
+	scanf("%d",&b);
+	
+	if(a>b)
+	{
+		printf("\n first no is max");
+	}
+	else
+	{
+		printf("\n Second no is max");
+	}
+}
