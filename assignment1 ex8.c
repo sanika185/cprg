6d4f56d307742434ee086190582c31897f3dfeb4
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-09-30 13:27:14 +0530

    Add files via upload

---
+++ assignment1 ex8.c
@@ -0,0 +1,13 @@
+//Ass:wap to input side of sqare and calculate the area//
+
+#include<stdio.h>
+void main()
+
+{
+	int side,area;
+	
+	printf("\n Enter side of square");
+	scanf("%d",&side);
+	area=side*side;
+	printf("\n Area of square=%d",area);
+}
