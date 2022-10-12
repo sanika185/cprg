404839ac26ba3089dc39f9d7113e899f370981c7
sanika185 <113962395+sanika185@users.noreply.github.com>
2022-10-12 14:08:50 +0530

    Add files via upload

---
+++ pizza.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int choice,qtys,amount;
+	char size[20];
+	
+	printf("\n choice:");
+	scanf("%d",&choice);
+	printf("\n quantity:");
+	scanf("%d",&qtys);
+	printf("\n Size:");
+	scanf("%s",&size);
+	
+	switch(strcmp(size,"small")==0)
+	{
+		switch(choice)
+		{
+			case 1:
+				amount=qtys*250;
+				break;
+			case 2:
+				amount=350*qtys;
+			    break;
+			case 3:
+				amount=150*qtys;
+				break;
+			case 4:
+				amount=400*qtys;
+				break;
+			case 5:
+				amount=550*qtys;
+				break;
+				
+     	}
+	}
+	switch (strcmp(size,"medium")==0)
+	{
+		switch(choice)
+		{
+			case 1:
+				amount=qtys*250+(250*10/100);
+				break;
+			case 2:
+				amount=qtys*350+(350*10/100);
+				break;
+			case 3:
+				amount=qtys*150+(150*10/100);
+				break;
+			case 4:
+				amount=qtys*400+(400*10/100);
+			    break;
+			case 5:
+				amount=qtys*550+(550*10/100);
+				break;
+		}
+	}
+	switch(strcmp(size,"large")==0)
+	{  
+	     switch(choice)
+	     {
+	     	case 1:
+				amount=qtys*250+(250*20/100);
+				break;
+			case 2:
+				amount=qtys*350+(350*20/100);
+				break;
+			case 3:
+				amount=qtys*150+(150*20/100);
+				break;
+			case 4:
+				amount=qtys*400+(400*20/100);
+			    break;
+			case 5:
+				amount=qtys*550+(550*20/100);
+				break;
+	     	
+		 }
+	    
+	}
+	system("cls");
+	printf("\n>>>>>Bill<<<<<");
+	printf("\n size:%s",size);
+	printf("\n Total amount=%d",amount);
+	       
+}
